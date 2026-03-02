13ad58d329c57b41cd792a29de74afa3754905dd
jaswanthchallagit28 <jaswanthchalla28@gmail.com>
2026-03-02 14:16:17 +0530

    Added UC1

---
+++ OppsBannerApp.java
@@ -0,0 +1,15 @@
+
+/**
+ * OOPSBannerApp
+ * Prints OOPS to console
+ * @author jaswanth
+ * @version 1.0
+ */
+
+public class OppsBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
